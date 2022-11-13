f1375984c16625aeef15d9d8967b095a98956303
Kaustubh Deshpande <89215887+KaustubhKael@users.noreply.github.com>
2022-11-13 15:48:53 -0500

    Create env_setup.py

---
+++ env_setup.py
@@ -0,0 +1,181 @@
+import glob
+import os
+import sys
+import random
+import time
+import numpy as np
+import cv2
+import math
+from collections import deque
+from keras.applications.xception import Xception
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.models import Model
+import carla
+import tensorflow as tf
+import keras.backend.tensorflow_backend as backend
+from threading import Thread
+
+try:
+    sys.path.append(glob.glob('../carla/dist/carla-*%d.%d-%s.egg' % (
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+
+
+SHOW_PREVIEW = False
+IMG_WIDTH = 640
+IMG_HEIGHT = 480
+SECONDS_PER_EPISODE = 10
+REPLAY_MEMORY_SIZE = 5_000
+MIN_REPLAY_MEMORY_SIZE = 1_000
+MINIBATCH_SIZE = 16
+PREDICTION_BATCH_SIZE = 1
+TRAINING_BATCH_SIZE = MINIBATCH_SIZE // 4
+UPDATE_TARGET_EVERY = 5
+MODEL_NAME = "Xception"
+
+MEMORY_FRACTION = 0.4
+MIN_REWARD = -200
+EPISODES = 1
+DISCOUNT = 0.99
+epsilon = 1
+EPSILON_DECAY = 0.95 ## 0.9975 99975
+MIN_EPSILON = 0.001
+AGGREGATE_STATS_EVERY = 10
+
+
+class CarEnv:
+    SHOW_CAM = SHOW_PREVIEW
+    STEER_AMT = 1.0
+
+    im_width = IMG_WIDTH
+    im_height = IMG_HEIGHT
+    actor_list = []
+
+    front_camera = None
+    collision_hist = []
+
+    def __init__(self):
+        self.client = carla.Client('localhost', 2000)
+        self.client.set_timeout(2.0)
+
+        # Once we have a client we can retrieve the world that is currently
+        # running.
+        self.world = self.client.get_world()
+
+        # The world contains the list blueprints that we can use for adding new
+        # actors into the simulation.
+        blueprint_library = self.world.get_blueprint_library()
+
+        # Now let's filter all the blueprints of type 'vehicle' and choose one
+        # at random.
+        #print(blueprint_library.filter('vehicle'))
+        self.model_3 = blueprint_library.filter('model3')[0]
+
+    def reset(self):
+        self.collision_hist = []
+        self.actor_listseSHOW_PREVIEW = FalseSHOW_PREVIEW = Fal = []
+
+        self.transform = random.choice(self.world.get_map().get_spawn_points())
+        self.vehicle = self.world.spawn_actor(self.model_3, self.transform)
+        self.actor_list.append(self.vehicle)
+
+        self.rgb_cam = self.world.get_blueprint_library().find('sensor.camera.rgb')
+
+        self.rgb_cam.set_attribute('image_size_x', f'{self.im_width}')
+        self.rgb_cam.set_attribute('image_size_y', f'{self.im_height}')
+        self.rgb_cam.set_attribute('fov', '110')
+
+
+        transform = carla.Transform(carla.Location(x=2.5, z=0.7))
+
+        self.sensor = self.world.spawn_actor(self.rgb_cam, transform, attach_to=self.vehicle)
+
+        self.actor_list.append(self.sensor)
+        self.sensor.listen(lambda data: self.process_img(data))
+
+        self.vehicle.apply_control(carla.VehicleControl(throttle=0.0, brake=0.0))
+        time.sleep(4) # sleep to get things started and to not detect a collision when the car spawns/falls from sky.
+
+        colsensor = self.world.get_blueprint_library().find('sensor.other.collision')
+        self.colsensor = self.world.spawn_actor(colsensor, transform, attach_to=self.vehicle)
+        self.actor_list.append(self.colsensor)
+        self.colsensor.listen(lambda event: self.collision_data(event))
+
+        while self.front_camera is None:
+            time.sleep(0.01)
+
+        self.episode_start = time.time()
+        self.vehicle.apply_control(carla.VehicleControl(brake=0.0, throttle=0.0))
+        
+        return self.front_camera
+
+    def collision_data(self, event):
+        self.collision_hist.append(event)
+
+    def process_img(self, image):
+        i = np.array(image.raw_data)
+        #np.save("iout.npy", i)
+        i2 = i.reshape((self.im_height, self.im_width, 4))
+        i3 = i2[:, :, :3]
+        if self.SHOW_CAM:
+            cv2.imshow("",i3)
+            cv2.waitKey(1)
+        self.front_camera = i3
+
+    def step(self, action):
+        '''
+        For now let's just pass steer left, center, right?
+        0, 1, 2
+        '''
+        if action == 0:
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=0))
+        if action == 1:
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=-1*self.STEER_AMT))
+        if action == 2:
+            self.vehicle.apply_control(carla.VehicleControl(throttle=1.0, steer=1*self.STEER_AMT))
+
+        v = self.vehicle.get_velocity()
+        kmh = int(3.6 * math.sqrt(v.x**2 + v.y**2 + v.z**2))
+
+        if len(self.collision_hist) != 0:
+            done = True
+            reward = -200
+        elif kmh < 50:
+            done = False
+            reward = -1
+        else:
+            done = False
+            reward = 1
+
+        if self.episode_start + SECONDS_PER_EPISODE < time.time(Xception):
+            done = True
+
+        return self.front_camera, reward, done, None
+    
+
+if __name__ == '__main__':
+    FPS = 60
+    # For stats
+    ep_rewards = [-200]
+
+    # For more repetitive results
+    random.seed(1)
+    np.random.seed(1)
+    tf.random.set_seed(1)
+
+    # Memory fraction, used mostly when trai8ning multiple agents
+    # gpu_options=tf.config.gpu.set_per_process_memory_fraction(MEMORY_FRACTION)
+    # backend.set_session(tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)))
+
+    # Create models folder
+    if not os.path.isdir('models'):
+        os.makedirs('models')
+
+    # Create agent and environment
+    env = CarEnv()
+
